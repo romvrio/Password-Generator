4ad1761a8e83dbbba0958ae39ee9590ee0b2066f
Romario <morrisromo9966@gmail.com>
2021-12-09 14:39:17 -0500

    added criteria for password and if statements

--- script.js
+++ script.js
@@ -7,13 +7,38 @@ var numeric = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
+function writePassword(generatePassword) {
+  //repeat and execute until conditions are met
+  while (passwordText <= 8 && passwordText >= 128) {
+    //inform user that password can not exceed these lengths
+    window.alert = ("Password must be at least 8-128 charachters long");
+  }
+
+  // Let user know what password can in include
+  if (generateBtn = "click") {
+    window.alert = ("Password can include " + upperCase + lowerCase + specialCharacters + numeric);
+  }
+
+  // If yes add uppercase, then add uppercase
+  if (upperCase === "yes" || upperCase === "YES") {
+    window.confirm("YES add Uppercase letters")
+    // Confirm that uppercase was selected
+    window.alert = ("Uppercase letters have been chosen")
+    // add in uppercase letters
+    console.log(["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"], upperCase);
+  }
+
+  else {
+    window.alert = (" Password is too short")
+  }
   var passwordText = document.querySelector("#password");
+  var password = generatePassword();
 
   passwordText.value = password;
 
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+for (var i = 0; i < generateBtn.length; i++) {
+  generateBtn.addEventListener("click", writePassword);
+}
