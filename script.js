df9e641551c65908362c5c09530ee7b2cd23ec89
Romario <morrisromo9966@gmail.com>
2021-12-10 12:56:46 -0500

    Edits to my if statments and arrays

--- script.js
+++ script.js
@@ -2,28 +2,35 @@
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "'", "/", "(", ":", ";", "<", ">", "=", "-", "_", "]", "[", "?", ".", "|"];
-var numeric = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+var numeric = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var content = [];
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function generate() {
-  var passwordType = window.prompt("How many characters would you like?");
-
-  if (passwordType < 8 || passwordType > 128) {
+function generatePassword() {
+  var passwordLength = window.prompt("How many characters would you like?");
+  console.log(passwordLength);
+  let passwordValue = "";
+  if (passwordLength < 8 || passwordLength > 128) {
     //inform user that password can not exceed these lengths
     window.alert("Password must be at least 8-128 charachters long");
-    passwordType = window.prompt("How many characters would like?");
-    //Let user know what password can in include
-    upperCase = window.confirm("Would you like Uppercase letters?");
-    lowerCase = window.confirm("Would you like lowercase letters?");
-    specialCharacters = window.confirm("Would you like Special characters");
-    numeric = window.confirm("Would you like Numebers?");
+    generatePassword();
   }
+
+  upperCase = window.confirm("Would you like Uppercase letters?");
+  lowerCase = window.confirm("Would you like lowercase letters?");
+  specialCharacters = window.confirm("Would you like Special characters");
+  numeric = window.confirm("Would you like Numebers?");
+
+  if (upperCase) {
+    //How to add empty array to contents arrays 
+  }
+
+  // do the same thing from line 27 to other arrays
 }
-if (upperCase = confirm) {
-  var generate = upperCase("", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",);
-}
+
+
 //If yes add uppercase, then add uppercase
 // if (upperCase === "yes" || upperCase === "YES") {
 // window.confirm("YES add Uppercase letters")
@@ -33,14 +40,22 @@ if (upperCase = confirm) {
 // console.log(["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"], upperCase);
 // }
 
+
 // else {
 // window.alert = (" Password is too short")
 // }
-var passwordText = document.querySelector("#generate");
-var password = generatePassword();
 
-// passwordText.value = password;
-//}
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate")
 
-//Add event listener to generate button
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
