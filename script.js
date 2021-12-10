1aa48286a62b45b4e4dfde8d633d64a4de4bcdb0
Romario <morrisromo9966@gmail.com>
2021-12-10 01:12:42 -0500

    assigned variables and wrote function

--- script.js
+++ script.js
@@ -7,38 +7,40 @@ var numeric = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword(generatePassword) {
-  //repeat and execute until conditions are met
-  while (passwordText <= 8 && passwordText >= 128) {
+function generate() {
+  var passwordType = window.prompt("How many characters would you like?");
+
+  if (passwordType < 8 || passwordType > 128) {
     //inform user that password can not exceed these lengths
-    window.alert = ("Password must be at least 8-128 charachters long");
+    window.alert("Password must be at least 8-128 charachters long");
+    passwordType = window.prompt("How many characters would like?");
+    //Let user know what password can in include
+    upperCase = window.confirm("Would you like Uppercase letters?");
+    lowerCase = window.confirm("Would you like lowercase letters?");
+    specialCharacters = window.confirm("Would you like Special characters");
+    numeric = window.confirm("Would you like Numebers?");
   }
-
-  // Let user know what password can in include
-  if (generateBtn = "click") {
-    window.alert = ("Password can include " + upperCase + lowerCase + specialCharacters + numeric);
-  }
-
-  // If yes add uppercase, then add uppercase
-  if (upperCase === "yes" || upperCase === "YES") {
-    window.confirm("YES add Uppercase letters")
-    // Confirm that uppercase was selected
-    window.alert = ("Uppercase letters have been chosen")
-    // add in uppercase letters
-    console.log(["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"], upperCase);
-  }
-
-  else {
-    window.alert = (" Password is too short")
-  }
-  var passwordText = document.querySelector("#password");
-  var password = generatePassword();
-
-  passwordText.value = password;
-
 }
-
-// Add event listener to generate button
-for (var i = 0; i < generateBtn.length; i++) {
-  generateBtn.addEventListener("click", writePassword);
+if (upperCase = confirm) {
+  var generate = upperCase("", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",);
 }
+//If yes add uppercase, then add uppercase
+// if (upperCase === "yes" || upperCase === "YES") {
+// window.confirm("YES add Uppercase letters")
+// Confirm that uppercase was selected
+// window.alert = ("Uppercase letters have been chosen")
+//add in uppercase letters
+// console.log(["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"], upperCase);
+// }
+
+// else {
+// window.alert = (" Password is too short")
+// }
+var passwordText = document.querySelector("#generate");
+var password = generatePassword();
+
+// passwordText.value = password;
+//}
+
+//Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
