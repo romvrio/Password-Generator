a8371478dfe1ebfe876544595bb3eea8b0418c9e
Romario <morrisromo9966@gmail.com>
2021-12-12 22:20:04 -0500

    added completed if statemts

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // Assignment code here
-var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "'", "/", "(", ":", ";", "<", ">", "=", "-", "_", "]", "[", "?", ".", "|"];
-var numeric = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-var content = [];
+let upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+let lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+let specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "'", "/", "(", ":", ";", "<", ">", "=", "-", "_", "]", "[", "?", ".", "|"];
+let numeric = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+let content = [];
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -12,6 +12,7 @@ function generatePassword() {
   var passwordLength = window.prompt("How many characters would you like?");
   console.log(passwordLength);
   let passwordValue = "";
+
   if (passwordLength < 8 || passwordLength > 128) {
     //inform user that password can not exceed these lengths
     window.alert("Password must be at least 8-128 charachters long");
@@ -23,27 +24,29 @@ function generatePassword() {
   specialCharacters = window.confirm("Would you like Special characters");
   numeric = window.confirm("Would you like Numebers?");
 
-  if (upperCase) {
-    //How to add empty array to contents arrays 
+  if (upperCase = "yes") {
+    let generateUpperCase = upperCase[Math.floor(Math.random() * upperCase.length - 1)]
+    console.log(generateUpperCase);
   }
 
-  // do the same thing from line 27 to other arrays
+  if (passwordValue = lowerCase) {
+    let generateLowerCase = lowerCase[Math.floor(Math.random() * lowerCase.length - 1)]
+    console.log(generateLowerCase);
+  }
+
+  if (passwordValue = specialCharacters) {
+    let generateSpecialCharacters = specialCharacters[Math.floor(Math.random() * specialCharacters.length - 1)]
+    console.log(generateSpecialCharacters);
+  }
+
+  if (passwordValue = numeric) {
+    let generateNumeric = numeric[Math.floor(Math.random() * numeric.length - 1)]
+    console.log(generateNumeric);
+
+
+  }
+
 }
-
-
-//If yes add uppercase, then add uppercase
-// if (upperCase === "yes" || upperCase === "YES") {
-// window.confirm("YES add Uppercase letters")
-// Confirm that uppercase was selected
-// window.alert = ("Uppercase letters have been chosen")
-//add in uppercase letters
-// console.log(["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"], upperCase);
-// }
-
-
-// else {
-// window.alert = (" Password is too short")
-// }
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate")
@@ -59,3 +62,7 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+for (i = 0; i < generatePassword.length; i++) {
+  password[Value];
+}
